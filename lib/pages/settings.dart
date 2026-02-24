b691ac2d8ea4b0633d983d41c0d872a1d416ba21
Nicole Lourenço <nicolelourenco38@gmail.com>
2026-02-24 09:42:12 -0300

    Update settings.dart

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -329,7 +329,7 @@ class _SettingsPageState extends State<SettingsPage> {
   static const String _mitLicenseText = '''
 MIT License
 
-Copyright (c) 2026 André Luferat / ONCredit
+Copyright (c) 2026 Nicole Lourenço / ONCredit
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
